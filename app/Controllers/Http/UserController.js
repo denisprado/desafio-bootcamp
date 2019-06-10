6672ecdfc384e3d05b6c6088cbf8f18daf03dfda
denisprado <denisforigo@gmail.com>
2019-06-10 16:27:19 -0300

    first commit

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async index() {
+    const user = await User.all()
+
+    return user
+  }
+
+  async store({ request }) {
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
+  }
+}
+
+module.exports = UserController
